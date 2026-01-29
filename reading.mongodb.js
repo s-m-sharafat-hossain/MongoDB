90a8b38294a9b4ceccf2edc2b2c80c4a2a888e1a
s-m-sharafat-hossain <sharafatcox60@gmail.com>
2026-01-29 08:43:18 +0600

    basic Operation

--- reading.mongodb.js
+++ reading.mongodb.js
@@ -1,4 +1,15 @@
 use('FirstDb');
 
-db.products.find({name: "Laptop"});
+//db.products.find({name: "Laptop"});
 //db.Contacts.find({"name": "John Doe"})
+
+// Use $gt operator to find products with price greater than 1000
+//db.products.find({price: {$gt: 1000}});
+
+
+// Use $gte to $lte operator to find products with price between 1000 and 2000
+//db.products.find({price: {$gte: 500, $lte: 1000}});
+
+//We can add two condition
+
+db.products.find({price: {$gte: 500, $lte: 1000}, name: "Laptop"});
